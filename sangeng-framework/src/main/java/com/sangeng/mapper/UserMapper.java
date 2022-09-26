17d743fc06d76b75e590ca7a3d088e4cb709f187
SXL1218 <1269152307@qq.com>
2022-09-26 15:59:17 +0800

    用户登录

---
+++ sangeng-framework/src/main/java/com/sangeng/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.sangeng.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sangeng.domain.entity.User;
+
+
+/**
+ * 用户表(User)表数据库访问层
+ *
+ * @author makejava
+ * @since 2022-09-26 14:30:40
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
