695ce071258456a2001e0dc7d9f2f493b882e479
SXL1218 <1269152307@qq.com>
2022-10-10 17:32:57 +0800

    统一异常处理

--- sangeng-blog/src/main/java/com/sangeng/config/SecurityConfig.java
+++ sangeng-blog/src/main/java/com/sangeng/config/SecurityConfig.java
@@ -10,17 +10,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
+
+    @Override
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
+
     @Autowired
     private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+    @Autowired
+    AuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    AccessDeniedHandler accessDeniedHandler;
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -32,19 +41,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/login").anonymous()
+                //jwt过滤器测试用，如果测试没有问题吧这里删除了
                 .antMatchers("/link/getAllLink").authenticated()
                 // 除上面外的所有请求全部不需要认证即可访问
                 .anyRequest().permitAll();
 
+        //配置异常处理器
+        http.exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler);
 
         http.logout().disable();
+        //把jwtAuthenticationTokenFilter添加到SpringSecurity的过滤器链中
         http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         //允许跨域
         http.cors();
     }
-    @Override
+
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- sangeng-blog/src/main/java/com/sangeng/filter/JwtAuthenticationTokenFilter.java
+++ sangeng-blog/src/main/java/com/sangeng/filter/JwtAuthenticationTokenFilter.java
@@ -21,47 +21,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
-
 @Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
     @Autowired
     private RedisCache redisCache;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
         //获取请求头中的token
-        String token=request.getHeader("token");
-
-        //解析获取的userid
-        if (!StringUtils.hasText(token)){
-            //说明该接口不需要登录，直接放行
-            filterChain.doFilter(request,response);
+        String token = request.getHeader("token");
+        if(!StringUtils.hasText(token)){
+            //说明该接口不需要登录  直接放行
+            filterChain.doFilter(request, response);
             return;
         }
-        Claims claims=null;
+        //解析获取userid
+        Claims claims = null;
         try {
-             claims= JwtUtil.parseJWT(token);
+            claims = JwtUtil.parseJWT(token);
         } catch (Exception e) {
             e.printStackTrace();
-            //token超时 token非法
-            //响应告诉前端重新登录
-            ResponseResult result=ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+            //token超时  token非法
+            //响应告诉前端需要重新登录
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
             WebUtils.renderString(response, JSON.toJSONString(result));
             return;
         }
-        String userId=claims.getSubject();
-        //从Redis中获取用户信息
-        LoginUser loginUser= redisCache.getCacheObject("bloglogin"+userId);
-        if (Objects.isNull(loginUser)){
-            //登录过期 提示重新登录
-            ResponseResult result=ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+        String userId = claims.getSubject();
+        //从redis中获取用户信息
+        LoginUser loginUser = redisCache.getCacheObject("bloglogin:" + userId);
+        //如果获取不到
+        if(Objects.isNull(loginUser)){
+            //说明登录过期  提示重新登录
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
             WebUtils.renderString(response, JSON.toJSONString(result));
             return;
         }
-        //存入securityContextHolder
-        UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(loginUser,null,null);
+        //存入SecurityContextHolder
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser,null,null);
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        filterChain.doFilter(request,response);
+
+        filterChain.doFilter(request, response);
     }
+
+
 }
